0e398ba323805aad57567c802db13261d27d553a
Kirill <kirillgovorov@gmail.com>
2023-07-13 23:58:46 +0300

    задача 15 готова, задачу 13 не смог

---
+++ Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("введите любое трехзначное число");
+int i = Convert.ToInt32(Console.ReadLine());
+int thirdDigit;
+
+
+if (i < 0) 
+{
+    i = i * -1;
+} 
+char[] arr = i.ToString().ToCharArray();
+
+if (Array < 2)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+     
+else 
+{
+     thirdDigit = Array[2];
+     Console.WriteLine((thirdDigit));
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("введите цифру дня недели, я проверю, является ли этот день выходным");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 0 || number > 7) 
+{
+    Console.WriteLine("нет такого дня недели");
+}
+
+else 
+{
+    if (number==6 || number==7) 
+    {
+        Console.WriteLine("да");
+    }
+
+    else 
+    {
+        Console.WriteLine("нет");
+    }
+}
